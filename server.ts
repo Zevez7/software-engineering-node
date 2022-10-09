dd4c0090540416b7ebd7fc278cfef7bcc64a09a5
Zevez7 <datguy7@gmail.com>
2022-10-09 01:12:09 -0400

    commit node

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 
-const movieController = new MoviesController(app);
+// const movieController = new MoviesController(app);
 
 const userController = new UserController(app, new UserDao());
 
@@ -22,26 +22,3 @@ const tuitController = new TuitController(app, new TuitDao());
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
-
-// const dbCallback = (movies: any) => {
-//   console.log("inovked when db returns data");
-//   console.log(movies);
-// };
-
-// moviesDao.findAllMovies().then((movies) => {
-//   console.log(movies);
-// });
-
-// function sayHello(req:Request, res:Response) {
-//     res.send("Hi from FSD!!");
-
-// }
-
-// app.get("/hello", (req: Request, res: Response) =>
-//   res.send("Welcome to Foundation of Software Engineering!")
-// );
-
-/**
- * Start a server listening at port 4000 locally
- * but use environment variable PORT on Heroku if available.
- */
