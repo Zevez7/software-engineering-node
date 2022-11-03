21fe044123a3784ac6909a41ec849a5314c4b26f
Zevez7 <datguy7@gmail.com>
2022-11-03 17:26:35 -0400

    some changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const app = express();
-
 /**
  * @file server file to run the ports
  */
+
+const express = require("express");
+const app = express();
+
 app.get("/hello", (req, res) => res.send("Hello World!"));
 
 const PORT = 5000;
